a4a39ce7490895709e8c57330beb3f0c64733d75
Opilane TTHK <opilane@tthk.ee>
2021-10-26 14:06:20 +0300

    Week9-wishList

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootdirectory = @"C:\Users\opilane\samples";
+            string userdirevtory = "wishlist";
+
+            string newDirctoryFullPath = @$"{rootdirectory}\{userdirevtory}";
+
+            Console.WriteLine(newDirctoryFullPath);
+
+            Directory.CreateDirectory(newDirctoryFullPath);
+        }
+    }
+}
